67974954d05bceee17813390efbf3580b064ce25
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-19 05:33:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,10 @@
 
 class Dog
-  def initialize(dog_name)
+  def name=(dog_name)
     @name = dog_name
   end
   def name
     @name
-  end
-  def name=(new_dog_name)
-    @name = new_dog_name
   end
 
   def breed=(dog_breed)
